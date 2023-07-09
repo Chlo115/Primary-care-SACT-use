c86ce5843395301be65c20276752e00c8278f5f6
Calum Polwart <polc1410@gmail.com>
2023-07-09 18:31:52 +0100

    Add a loop which will download all the data of interest from open prescribing and save as an RDS file

---
+++ Download_and_Save_OpenPrescribingData.R
@@ -0,0 +1,53 @@
+# This script will download data from OpenPrescribing and Merge it into
+# a single file and save the output as an R RDS object ready for analysis
+#
+
+# Load some essential packages if they are not already loaded into this R session
+# If these are not already installed, they will required installation.
+require(tidyverse)
+require(httr)
+require(rvest)
+require(jsonlite)
+require(glue)
+
+# Load some functions to read the data.  Functions are snippets of code that are
+# re-used multiple times. You can pass pre-defined variables to functions
+#
+# This project is storing Functions in a folder called Functions.  Each function
+# has been saved in a file with the same name in that folder and is 'sourced'
+# to load it into R, so that it can be called when ready
+source ("Functions/get_openprescribing_data.R")
+
+## Example
+# get_openprescribing_data("0801030D0", "Capecitabine")
+# returns a tibble with data for capecitabine
+
+# We need to repeat that process for each drug we are intersted in, and combine them together
+
+## TO-DO - get the actual list we are interested in.
+## FOR NOW - here is a quick list
+codelist <- tibble(bnf_code = c("0801030D0", "0801050AA", "0801050CB"),
+                   drug = c("Capecitabine", "Imatinib", "Palbociclib"))
+
+# Loop through the code list...
+
+for (code in codelist$bnf_code) {
+    # get the data for this drug and save as drug_result
+    drug_result <- get_openprescribing_data(code, unique(codelist$drug[codelist$bnf_code == code]))
+
+    if (!exists("op_data")) {
+        # if op_data doesn't exist, create it from drug_result
+        op_data <- drug_result
+    } else {
+        # if op_data exists, add drug_result to it
+        op_data <- rbind(op_data, drug_result)
+    }
+}
+
+# remove the drug_result (it is a temporary data set)
+rm(drug_result)
+
+# Save the Open Precribing Data to an R RDS Object
+
+saveRDS(op_data, file = "Data/OpenPrescribing.RDS")
+
