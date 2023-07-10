8957fe28c14576e70296df4227449798c8bb0b6e
Chlo115 <chloe.waterson@nhs.net>
2023-07-10 22:01:57 +0100

    results update

--- analyse_openprescribing_data.R
+++ analyse_openprescribing_data.R
@@ -1,6 +1,7 @@
 #run some analysis
 #assume data already exists in object op_data
-
+require(tidyverse)
+require(cowplot)
 #restrict date range
 
 op_data %>%
@@ -12,3 +13,19 @@ total_items <- sum(op_data$Items, na.rm = TRUE)
 
 #Calculate total expenditure
 total_cost <- sum(op_data$Cost, na.rm = TRUE)
+
+op_data %>% 
+  group_by(Month) %>% 
+  summarise(Items=sum(Items),Cost=sum(Cost)) %>% 
+  ggplot(aes(x=Month,y=Cost))+
+  #geom_col(  )+
+  geom_line(aes(y=Cost))-> p1
+
+op_data %>% 
+  group_by(Month) %>% 
+  summarise(Items=sum(Items),Cost=sum(Cost)) %>% 
+  ggplot(aes(x=Month,y=Items))+
+  geom_col(  )-> p2
+  #geom_line(aes(y=Cost))
+
+plot_grid(p1, p2, ncol=1) -> fig1
