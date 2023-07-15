b30906cbc87d1c2d86d96778816e24e175cecd01
Calum Polwart <polc1410@gmail.com>
2023-07-15 17:43:46 +0100

    druglist to check for any missing selections

--- merge_drug_lists.R
+++ merge_drug_lists.R
@@ -1,10 +1,11 @@
 # Quick dirty code to merge lists
 
-install.packages("fuzzyjoin") # only run this once
-library(fuzzyjoin)
+#install.packages("stringr") # only run this once
+#library(fuzzyjoin)
 
 ATC <- readRDS("Data/ATC_codes.rds")
 
 ATC |>
-fuzzy_left_join ( codelist, by = c("Drug_name" = "Name")) |>
+    mutate(term = stringr::str_to_title(Name)) |>
+left_join ( codelist) |>
 write_csv( "druglist.csv")
