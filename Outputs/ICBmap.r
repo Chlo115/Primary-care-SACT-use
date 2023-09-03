dd58d534afb7139ed0f939ecc1be07e912994d83
Your Name <you@example.com>
2023-09-03 22:29:03 +0100

    more map

--- Outputs/ICBmap.r
+++ Outputs/ICBmap.r
@@ -20,6 +20,7 @@ require(grid)
 require(gridExtra)
 require(ggpubr)
 require(httr)
+require(tidyr)
 
 #' Get a "MapShape" from:
 #' https://geoportal.statistics.gov.uk/datasets/ons::sub-integrated-care-board-locations-july-2022-en-buc/explore?location=51.742731%2C-3.679550%2C5.60
@@ -40,10 +41,10 @@ require(httr)
 
 #' Read the geography into an object called 'shapefile'
 shapefile <-
-  readOGR(dsn = "MapShape", layer = "SICBL_JUL_2022_EN_BUC")
+  readOGR(dsn = "MapShape", layer = "ICB_JUL_2022_EN_BUC_V3")
 
 #' The shapefile needs converted to a different format using the following
-mapdata <- #tidy(shapefile, region = "cal19nm")
+mapdata_ICB<- #tidy(shapefile, region = "cal19nm")
 sf::st_as_sf(shapefile)
 #' Create some data to plot on the map (you could read a CSV file here etc)
 #' 
@@ -52,21 +53,43 @@ sf::st_as_sf(shapefile)
 Year_data |>
   group_by(Org_name,OrgId)|>
   summarise(Total_spend = sum(Cost))|>
-  mutate(Org_name=paste(Org_name,"ICB", "-",OrgId))-> spend_by_ICB
+  mutate(Org_name=paste(Org_name,"ICB", "-",OrgId))-> spend_by_subICB
 
+#' Read the geography into an object called 'shapefile'for sub ICBs
+shapefile <-  readOGR(dsn = "MapShape", layer = "SICBL_JUL_2022_EN_BUC")
 
+#' The shapefile needs converted to a different format using the following
+mapdata_subICB<- sf::st_as_sf(shapefile)
 
+mapdata_subICB |>
+  separate_wider_delim(SICBL22NM,delim=" - ", names=c("ICB","subICB"))-> mapdata_subICB
 
+mapdata_subICB |>
+  select(ICB,subICB) -> ICBnames
+  
+spend_by_subICB|>
+  dplyr::left_join(ICBnames, by=c("OrgId"="subICB")) -> spend_by_subICB
+
+spend_by_subICB |>
+  group_by(ICB)|>
+  summarise(Total_spend = sum(Total_spend))-> spend_by_ICB
 
 #' Join mydata with mapdata to create a single object (dataframe) called df
 #' which has -
 #'  - each cancer alliance
 #'  - its geography
 #'  - the heatmap value
-mapdata|>
-  mutate(SICBL22NM=toupper(SICBL22NM))->mapdata
+mapdata_ICB|>
+  mutate(ICB22NM=stringr::str_replace(ICB22NM,"Integrated Care Board","ICB"))->mapdata_ICB
 
-df <- dplyr::left_join(mapdata, spend_by_ICB, by=c("SICBL22NM"="Org_name"))
+df <- dplyr::left_join(mapdata_ICB, spend_by_ICB, by=c("ICB22NM"="ICB"))
+
+df|>
+  mutate(Total_spend=case_when(
+    is.na(Total_spend)~0,
+    T~Total_spend
+  )) -> df
+
 #' Create the left hand side map.  Maps are in fact "graphs" and produced using a package 
 #' called ggplot - its powerful but a little complex to explain...
 
@@ -99,4 +122,15 @@ plot1 <- plot1 + theme(axis.title.y=element_blank(),
                        axis.text.y = element_blank(), 
                        axis.ticks.y = element_blank())
 
+plot1 <- plot1 + scale_fill_gradient(low="white",high=rgb(45/255,60/255,80/255),
+                                     breaks=c(0,20000,40000,60000,80000),
+                                     labels=c("£0","£20K","£40K","£60K","£80K"))
+
+plot1 <- plot1 + labs(fill="Expenditure by ICB\n")
+
+plot1 <- plot1 + theme(legend.text=element_text(color="white"),legend.title=element_text(color="white"))
+
 print(plot1)
+
+ggsave("Spend_map.svg")
+
